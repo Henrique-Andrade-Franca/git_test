c0908730e6e3abb42e450f19cd7adf440c2868a1
Henrique-Andrade-Franca <henriqu3andradefranca@gmail.com>
2023-02-13 17:19:50 -0300

    Leitura de variavel

--- testando.cpp
+++ testando.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main(void){
 
+  int ap;
+
   cout << "Hello World!" << endl;
 
   for(int i = 0; i < 10; i++){
     cout << "Hello World!";
   }
+
+  cin >> ap; 
 
   return 0;
 }
